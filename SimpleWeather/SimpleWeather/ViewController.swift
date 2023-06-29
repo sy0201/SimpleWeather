b555470f726d1c5f7b603d62630d6dd0d2f71624
sy0201 <noey0201@gmail.com>
2023-06-29 18:31:46 +0900

    [INIT] 프로젝트 생성

---
+++ SimpleWeather/SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by siyeon park on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

4672e0a6f0c8ec5bcace1c750a1821427532888d
sy0201 <noey0201@gmail.com>
2023-06-30 15:45:15 +0900

    [FEAT] 현재 위치 위도, 경도 받아오기

--- SimpleWeather/SimpleWeather/ViewController.swift
+++ SimpleWeather/SimpleWeather/ViewController.swift
@@ -6,13 +6,50 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    var locationManager = CLLocationManager()
+    var currentLocation:CLLocationCoordinate2D!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        requestAuthorization()
+
         NetworkingManager.shared.requestWeather(baseDate: "20230630", nx: "55", ny: "127") {
         }
+    }
+}
+
+private extension ViewController {
+    func requestAuthorization() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()// 거리 정확도
+        locationManager.requestLocation()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+        else {
+            print("위치 서비스 허용 off")
+        }
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("위치 업데이트!")
+            print("위도 : \(location.coordinate.latitude)")
+            print("경도 : \(location.coordinate.longitude)")
+            NetworkingManager.shared.requestWeather(baseDate: "20230630", nx: "\(location.coordinate.latitude)", ny: "\(location.coordinate.longitude)") {
+            }
+        }
+    }
+
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
     }
 }
